bc3a490adeb213a5ecc0a72a7262730791bce097
Raphaël Defaix <raphzdef@gmail.com>
2026-07-31 17:21:22 +0100

    Update run_alpha_statistics.R

--- Scripts/run_alpha_statistics.R
+++ Scripts/run_alpha_statistics.R
@@ -13,6 +13,34 @@ run_alpha_statistics <- function(
 
   results <- list()
 
+  ## Days with at least two groups
+  days_to_compare <- alpha_long %>%
+    filter(.data[[group_var]] != "untreated") %>%
+    distinct(.data[[day_var]]) %>%
+    pull()
+
+  for(index in alpha_indices){
+
+    message("-------------------------")
+    message("Index : ", index)
+
+    for(day in days_to_compare){
+
+      df_test <- alpha_long %>%
+        filter(
+          Index == index,
+          .data[[day_var]] == day,
+          .data[[group_var]] != "untreated"
+        ) %>%
+        droplevels()
+
+      message("   Day : ", day,
+              " (n = ", nrow(df_test), ")")
+
+    }
+
+  }
+
   return(results)
 
 }
